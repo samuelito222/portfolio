563cd1c4364998f1f10d2d7d8166412945032b1c
samuelito222 <samuelitomejor@protonmail.com>
2024-03-07 18:08:21 -0600

    Juiced to the flow

--- js/somefile.js
+++ js/somefile.js
@@ -6,3 +6,24 @@ const collapseElementList = document.querySelectorAll('.collapse')
 const collapseList = [...collapseElementList].map(collapseEl => new bootstrap.Collapse(collapseEl))
 
 console.log(collapseList);
+
+
+const alertPlaceholder = document.getElementById('liveAlertPlaceholder');
+const appendAlert = (message, type) => {
+    const wrapper = document.createElement('div');
+    wrapper.innerHTML = [
+     ` <div class="alert alert-${type} alert-dismissable" role="alert">`,
+     ` <div>${message}</div>`,
+     ' <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+    '</div>'
+].join('')
+
+alertPlaceholder.append(wrapper);
+}
+
+const alertTrigger = document.getElementById('liveAlertBtn');
+if (alertTrigger) {
+alertTrigger.addEventListener('click',() => {
+appendAlert('Nice, you did something!', 'success')
+})
+}
