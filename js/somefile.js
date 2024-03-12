8a69fe4949bbfc501ccb931fd9b724381e3d6868
samuelito222 <samuelitomejor@protonmail.com>
2024-03-11 19:04:10 -0500

    Just about done with all the components

--- js/somefile.js
+++ js/somefile.js
@@ -1,4 +1,30 @@
 
+const scrollSpy = new bootstrap.ScrollSpy(document.body, {
+    target: '#list-example'
+});
+
+
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
+
+
+const popover = new bootstrap.Popover('.example-popover', {
+  container: 'body'
+})
+
+const toastTrigger = document.getElementById('liveToastBtn');
+const toastLiveExample = document.getElementById('liveToast');
+
+console.log("here");
+if (toastTrigger) {
+  const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample);
+  toastTrigger.addEventListener('click',() => {
+    toastBootstrap.show();
+  });
+  console.log("there");
+}
+
+
 console.log("5");
 
 
@@ -16,14 +42,18 @@ const appendAlert = (message, type) => {
      ` <div>${message}</div>`,
      ' <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
     '</div>'
-].join('')
+].join('');
 
 alertPlaceholder.append(wrapper);
 }
 
+
+console.log("foo foo");
 const alertTrigger = document.getElementById('liveAlertBtn');
 if (alertTrigger) {
 alertTrigger.addEventListener('click',() => {
 appendAlert('Nice, you did something!', 'success')
 })
 }
+
+
